41a3d0c67aadde8f033816d1ecd7ef0a3d0a2249
Kalelkar-Noman <kalelkarnoman014@gmail.com>
2024-01-11 17:48:29 +0530

    tweaks

--- index.Master.cs
+++ index.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,8 @@ namespace Food_Delivery_Website
 {
     public partial class Index : System.Web.UI.MasterPage
     {
-        readonly SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\KNA\\Documents\\Projects_College\\Food_Delivery_Website\\DB\\Food_Delivery.mdf;Integrated Security=True;Connect Timeout=30");
-
+        //readonly SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Projects\\Food_Delivery_Website_Asp.net\\DB\\Food_Delivery.mdf;Integrated Security=True;Connect Timeout=30");
+        readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Food_DeliveryConnectionString"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
            
